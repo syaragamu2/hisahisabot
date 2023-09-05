804aabe53ba36ff2ece8be555db40cdf8b2131de
syaragamu2 <takashisyaragamu@gmail.com>
2023-09-06 00:31:12 +0900

    settingsへんこう

--- linebot/settings.py
+++ linebot/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*71_$e5wxpj=3$+yr)e_l)pjc4(d4tp0@fqi10m@w*rmnstqil'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -116,10 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static_local" ]
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / "media_local"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
