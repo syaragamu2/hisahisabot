7bd43d06e18bf197424bd7fb249969d355703820
syaragamu2 <takashisyaragamu@gmail.com>
2024-01-16 22:57:20 +0900

    リンク変更

--- comicsearch/bot_comics.py
+++ comicsearch/bot_comics.py
@@ -10,12 +10,11 @@ def create_message(msg):
 def create_comic_url(title):
     # ここで漫画のタイトルを元にURLを生成するロジックを実装
     # 例: "One Piece" の場合、該当URLを生成する処理を記述
-    comic_url = "https://manga1001.tv/?s=" + title
+    comic_url = "https://mangaraw.plus/filter?name=" + title
 
     # 該当のURLが見つからない場合は None を返す
     return comic_url
 
-def 
 
 
 
