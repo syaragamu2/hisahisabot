24c876a4a3c1f4f56e5ce0a58b078ddb2291bcb6
syaragamu2 <takashisyaragamu@gmail.com>
2024-10-01 22:31:45 +0900

    debugtrue

--- linebot/settings.py
+++ linebot/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*71_$e5wxpj=3$+yr)e_l)pjc4(d4tp0@fqi10m@w*rmnstqil'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
