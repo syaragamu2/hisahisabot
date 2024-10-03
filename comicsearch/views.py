e4f617f39934a4ab45f4c766e2f9f622e0cf293d
syaragamu <takashidoradora@gmail.com>
2024-10-03 23:00:23 +0900

    aaaaaa

--- comicsearch/views.py
+++ comicsearch/views.py
@@ -40,4 +40,5 @@ def linebot(request):
 
         return JsonResponse({'message': 'OK'}, status=200)
     else:
-        return JsonResponse({'message': 'Unsupported method'}, status=405)
+        #return JsonResponse({'message': 'Unsupported method'}, status=405)
+        return JsonResponse({'message': 'OK'}, status=200)
