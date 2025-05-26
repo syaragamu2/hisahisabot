8b2742dad6efc6f9ba4fa7a5db71c8ae68efa257
syaragamu <takashidoradora@gmail.com>
2025-05-26 23:09:33 +0900

    a

--- otakara/urls.py
+++ otakara/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import linebot_view
 urlpatterns = [
     #...
-    path("webhook/", linebot_view, name="otakara-webhook"),
+    path("webhook/", linebot_view, name="linebot_callback"),
 ]
